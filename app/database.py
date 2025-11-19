437325b90ccef0fcb586688b0c0d5a711da6cf54
felipeflosii <luizfelipeflosi@gmail.com>
2025-11-19 16:04:59 -0300

    feat: Inserção de dados QUESTIONS e AREAS no database da Oracle utilizando Pydantic V2;

--- app/database.py
+++ app/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.exc import SQLAlchemyError
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 ORACLE_USER = os.getenv("ORACLE_USER")
